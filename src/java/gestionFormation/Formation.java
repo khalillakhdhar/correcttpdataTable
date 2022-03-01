c49b15176e9781385bd4072a3cbdc275d99d0b4d
khalil <khalillakhdharatc@gmail.com>
2022-03-01 16:03:51 +0100

    formations

--- src/java/gestionFormation/Formation.java
+++ src/java/gestionFormation/Formation.java
@@ -5,10 +5,62 @@
  */
 package gestionFormation;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
 /**
  *
  * @author khali
  */
+@ManagedBean
+@RequestScoped
 public class Formation {
+    private String titre,description;
+    private int prix,nbjour;
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrix() {
+        return prix;
+    }
+
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+
+    public int getNbjour() {
+        return nbjour;
+    }
+
+    public void setNbjour(int nbjour) {
+        this.nbjour = nbjour;
+    }
+
+    public Formation() {
+    }
+
+    public Formation(String titre, String description, int prix, int nbjour) {
+        this.titre = titre;
+        this.description = description;
+        this.prix = prix;
+        this.nbjour = nbjour;
+    }
+    
+    
+    
     
 }
--- src/java/gestionFormation/Formations.java
+++ src/java/gestionFormation/Formations.java
@@ -5,10 +5,37 @@
  */
 package gestionFormation;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
 /**
  *
  * @author khali
  */
+
+
+@ManagedBean
+@RequestScoped
 public class Formations {
+    List<Formation> listFormations=new ArrayList<Formation>();
+
+    public List<Formation> getListFormations() {
+        Formation f1=new Formation("java", "java basique", 2000, 5);
+        Formation f2=new Formation("java JSF", "java JSF", 2000, 5);
+        Formation f3=new Formation("java hibernate", "java hibernate", 2000, 5);
+        listFormations.add(f1);
+        listFormations.add(f2);
+        listFormations.add(f3);
+        return listFormations;
+    }
+
+    public void setListFormations(List<Formation> listFormations) {
+        this.listFormations = listFormations;
+    }
+    
+    
+    
     
 }
